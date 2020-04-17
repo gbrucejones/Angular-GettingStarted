b89ef502ac1eb1f278d25a87b8cae92e417c8582
Gregory Jones <Gregory.Jones@sharp.com>
2020-04-16 22:57:20 -0700

    Module 13 complete

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,47 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AngularReactDatesModule } from 'angular-react-dates';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
-import { StarComponent } from './shared/star.component';
 import { AngularReactDateComponent } from './shared/angular-react-dates';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent
-    , ProductListComponent
-    , ConvertToSpacesPipe
-    , StarComponent
-    , AngularReactDateComponent
-    , ProductDetailComponent
     , WelcomeComponent
-    //, RouterModule
+    //, AngularReactDateComponent
   ],
   imports: [
     BrowserModule
-    , FormsModule
-    , AngularReactDatesModule.forRoot()
     , HttpClientModule
     , RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent }
-      ,{
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      }
-      , { path: 'welcome', component: WelcomeComponent }
+      { path: 'welcome', component: WelcomeComponent }
       , { path: '', redirectTo: 'welcome', pathMatch: 'full' }
       , { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
       //, { path: '**', component: pageNotFoundComponent }
     ], {useHash: true})
+    , ProductModule
+    //, AngularReactDatesModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
